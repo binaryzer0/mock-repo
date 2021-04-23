d2b0e534fad4ac7b9ece5861d0b8ec91df7d7aad
Jesper Eneberg <jesper@eneberg.nu>
2021-04-23 09:10:51 +0200

    sucess

--- Dockerfiles/suricata/docker-entrypoint.sh
+++ Dockerfiles/suricata/docker-entrypoint.sh
@@ -8,14 +8,14 @@ if [ "$enable_suricata_update" = true ]; then
         unset IFS
         su - suricata -s /bin/sh -c "suricata-update"
         crond
-        echo "0 * * * * su - suricata -s /bin/sh -c \"suricata-update update-sources && suricata-update --reload-command='suricatasc -c reload-rules'\" > /dev/null 2>&1" > /etc/crontabs/suricata-update-cron
+        echo "0 * * * * su - suricata -s /bin/sh -c \"suricata-update update-sources && suricata-update --reload-command='suricatasc -c reload-rules'\" > /dev/null 2>&1" > /etc/cron.d/suricata-update-cron
     else 
         echo "enabling suricata-update rules with no specified sources"
         su - suricata -s /bin/sh -c "suricata-update"
         crond
-        echo "0 * * * * su - suricata -s /bin/sh -c \"suricata-update --reload-command='suricatasc -c reload-rules'\" > /dev/null 2>&1" > /etc/crontabs/suricata-update-cron
+        echo "0 * * * * su - suricata -s /bin/sh -c \"suricata-update --reload-command='suricatasc -c reload-rules'\" > /dev/null 2>&1" > /etc/cron.d/suricata-update-cron
     fi
-    crontab /etc/crontabs/suricata-update-cron
+    crontab /etc/cron.d/suricata-update-cron
 fi
 
 exec /usr/bin/suricata --user suricata --group suricata -q 0 $@
