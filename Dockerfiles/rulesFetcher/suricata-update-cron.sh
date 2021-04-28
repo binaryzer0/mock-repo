47fa5f6a77597e156e61f4d1db9c34796b452da1
Jesper Eneberg <jesper@eneberg.nu>
2021-04-28 11:33:03 +0200

    refactor

--- Dockerfiles/rulesFetcher/suricata-update-cron.sh
+++ Dockerfiles/rulesFetcher/suricata-update-cron.sh
@@ -2,9 +2,27 @@
 
 #Update rules from rulesets
 rule_sources=$(aws ssm get-parameter --name $RulesetsSsmParameter --region $REGION --output text --query Parameter.Value 2> /dev/null)
+current_sources=$(cat /tmp/enabled.rules)
+
+#Fetches and updates sources from https://www.openinfosecfoundation.org/rules/index.yaml
 su - suricata -s /bin/sh -c "suricata-update update-sources --suricata-version 6.0.2"
-IFS=","; for I in $rule_sources; do su - suricata -s /bin/sh -c "suricata-update enable-source --suricata-version 6.0.2 $I"; done
-unset IFS
+
+oldIFS=$IFS
+IFS=","
+#Enabled new rule sources
+for I in $rule_sources; do su - suricata -s /bin/sh -c "suricata-update enable-source --suricata-version 6.0.2 $I"; done
+
+
+#Remove old rule sources
+for I in $current_sources; do 
+    if [[ ! "$rule_sources" =~ "$I" ]]; then
+        su - suricata -s /bin/sh -c "suricata-update remove-source --suricata-version 6.0.2 $I"
+    fi
+done
+
+IFS=$oldIFS
+echo $rule_sources > /tmp/enabled.rules
+
 su - suricata -s /bin/sh -c "suricata-update -f"
 
 #Download dynamic rules from s3
