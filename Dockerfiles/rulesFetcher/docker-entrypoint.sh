538d5218680913260c95c08029a334eeeb25571e
Jesper Eneberg <jesper@eneberg.nu>
2021-04-29 04:24:26 +0200

    refactor

--- Dockerfiles/rulesFetcher/docker-entrypoint.sh
+++ Dockerfiles/rulesFetcher/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-/suricata-update-cron.sh
+su -m suricata -s /bin/sh -c "/suricata-update-cron.sh $"
 
-echo "* * * * * su - suricata -s /bin/sh -c \"flock -n /tmp/cron.lock /suricata-update-cron.sh\" >/proc/1/fd/1 2>/proc/1/fd/2" > /etc/cron.d/suricata-update-cron.sh
+echo "* * * * * su -m suricata -s /bin/sh -c \"/bin/flock -n /tmp/cron.lock /suricata-update-cron.sh\" >/proc/1/fd/1 2>/proc/1/fd/2" > /etc/cron.d/suricata-update-cron.sh
 crontab /etc/cron.d/suricata-update-cron.sh
 
 exec crond -n 
--- Dockerfiles/rulesFetcher/suricata-update-cron.sh
+++ Dockerfiles/rulesFetcher/suricata-update-cron.sh
@@ -4,26 +4,29 @@
 rule_sources=$(aws ssm get-parameter --name $RulesetsSsmParameter --region $REGION --output text --query Parameter.Value 2> /dev/null)
 current_sources=$(cat /tmp/enabled.rules)
 
-#Fetches and updates sources from https://www.openinfosecfoundation.org/rules/index.yaml
-su - suricata -s /bin/sh -c "suricata-update update-sources --suricata-version 6.0.2"
-
 oldIFS=$IFS
 IFS=","
-#Enabled new rule sources
-for I in $rule_sources; do su - suricata -s /bin/sh -c "suricata-update enable-source --suricata-version 6.0.2 $I"; done
 
+if [[ -z "$rule_sources" ]]; then
+    #Fetches and updates sources from https://www.openinfosecfoundation.org/rules/index.yaml
+    suricata-update update-sources --suricata-version 6.0.2
+    #Enabled new rule sources
+    for I in $rule_sources; do 
+        suricata-update enable-source --suricata-version 6.0.2 $I; 
+    done
+fi
 
 #Remove old rule sources
 for I in $current_sources; do 
     if [[ ! "$rule_sources" =~ "$I" ]]; then
-        su - suricata -s /bin/sh -c "suricata-update remove-source --suricata-version 6.0.2 $I"
+        suricata-update remove-source --suricata-version 6.0.2 $I
     fi
 done
 
 IFS=$oldIFS
 echo $rule_sources > /tmp/enabled.rules
 
-su - suricata -s /bin/sh -c "suricata-update -f"
+suricata-update -f
 
 #Download dynamic rules from s3
 aws s3 cp s3://$DynamicRulesS3Path /var/lib/suricata/rules/dynamic.rules
