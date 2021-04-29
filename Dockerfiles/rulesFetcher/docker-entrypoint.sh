60483acd192283e01cda577709b2bcf1977ba8f7
Jesper Eneberg <jesper@eneberg.nu>
2021-04-29 18:30:54 +0200

    refactor

--- Dockerfiles/rulesFetcher/docker-entrypoint.sh
+++ Dockerfiles/rulesFetcher/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-su -m suricata -s /bin/sh -c "/suricata-update-cron.sh $"
+su - suricata -s /bin/sh -c "/suricata-update-cron.sh $RulesetsSsmParameter $REGION $DynamicRulesS3Path"
 
-echo "* * * * * su -m suricata -s /bin/sh -c \"/bin/flock -n /tmp/cron.lock /suricata-update-cron.sh\" >/proc/1/fd/1 2>/proc/1/fd/2" > /etc/cron.d/suricata-update-cron.sh
+echo "* * * * * su -m suricata -s /bin/sh -c \"/bin/flock -n /tmp/cron.lock /suricata-update-cron.sh $RulesetsSsmParameter $REGION $DynamicRulesS3Path\" >/proc/1/fd/1 2>/proc/1/fd/2" > /etc/cron.d/suricata-update-cron.sh
 crontab /etc/cron.d/suricata-update-cron.sh
 
 exec crond -n 
--- Dockerfiles/rulesFetcher/suricata-update-cron.sh
+++ Dockerfiles/rulesFetcher/suricata-update-cron.sh
@@ -1,32 +1,49 @@
 #!/bin/bash
+shopt -s extglob
+
+RulesetsSsmParameter=$1
+REGION=$2
+DynamicRulesS3Path=$3
 
 #Update rules from rulesets
-rule_sources=$(aws ssm get-parameter --name $RulesetsSsmParameter --region $REGION --output text --query Parameter.Value 2> /dev/null)
-current_sources=$(cat /tmp/enabled.rules)
+rule_sources_to_be_used=$(aws ssm get-parameter --name $RulesetsSsmParameter --region $REGION --output text --query Parameter.Value 2> /dev/null)
 
 oldIFS=$IFS
 IFS=","
 
-if [[ -z "$rule_sources" ]]; then
+#et/open is enabled by default. This function is a fix to remove et/open if it's not specified to be used. https://forum.suricata.io/t/suricata-update-use-only-local-rules/399/3 
+etOpenFix () {
+    if [[ ! "$rule_sources_to_be_used" = *et/open* ]]; then
+        echo "et/open not found in rule source list. Removing."
+        suricata-update remove-source --suricata-version 6.0.2 et/open
+    fi
+}
+
+removeRuleSources () {
+    for I in /var/lib/suricata/update/sources/*; do
+        source=$(grep "source:" $I | sed -e 's/source: //')
+        if [[ ! "$rule_sources_to_be_used" = *$source* ]]; then
+            suricata-update remove-source --suricata-version 6.0.2 $source
+        fi
+    done
+}
+
+if [[ ! -z "$rule_sources_to_be_used" ]]; then
     #Fetches and updates sources from https://www.openinfosecfoundation.org/rules/index.yaml
     suricata-update update-sources --suricata-version 6.0.2
     #Enabled new rule sources
-    for I in $rule_sources; do 
-        suricata-update enable-source --suricata-version 6.0.2 $I; 
+    for I in $rule_sources_to_be_used; do 
+        I="${I##*( )}"
+        suricata-update enable-source --suricata-version 6.0.2 $I
     done
+    removeRuleSources
+    etOpenFix
+else
+    removeRuleSources
+    etOpenFix
 fi
 
-#Remove old rule sources
-for I in $current_sources; do 
-    if [[ ! "$rule_sources" =~ "$I" ]]; then
-        suricata-update remove-source --suricata-version 6.0.2 $I
-    fi
-done
-
 IFS=$oldIFS
-echo $rule_sources > /tmp/enabled.rules
 
-suricata-update -f
-
-#Download dynamic rules from s3
 aws s3 cp s3://$DynamicRulesS3Path /var/lib/suricata/rules/dynamic.rules
+suricata-update -f --suricata-version 6.0.2 --url file:///dev/null
